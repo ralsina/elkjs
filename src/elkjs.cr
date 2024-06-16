6f14163bba94b15859ebf8d39ff325567e2416cf
Roberto Alsina <roberto.alsina@gmail.com>
2024-06-16 20:57:38 -0300

    Sintactic sugar to make defining JS funcs from Crystal nicer

--- src/elkjs.cr
+++ src/elkjs.cr
@@ -1,39 +1,4 @@
 # TODO: Write documentation for `Elkjs`
-module Js
-  VERSION = "0.1.0"
-
-  class Engine
-    def initialize(size : Int32 = 8192)
-      @js = Elk.js_create((" "*size), size)
-    end
-
-    def set_global(name : String, value : Elk::Jsval)
-      Elk.js_set(@js, Elk.js_glob(@js), name, value)
-    end
-
-    def eval(code : String)
-      Elk.js_eval(@js, code, code.bytesize)
-    end
-
-    def self.from_jsval(js : Elk::Js, val : Elk::Jsval)
-      # FIXME: handle JS_UNDEFINED, JS_ERR, JS_PRIV
-      case Elk.js_type(val)
-      when 1 # Elk::JsTypes::JS_NULL
-        Nil
-      when 2 # Elk::JsTypes::JS_TRUE
-        true
-      when 3 # Elk::JsTypes::JS_FALSE
-        false
-      when 4 # Elk::JsTypes::JS_STR
-        size : UInt64 = 0
-        buffer = Elk.js_getstr(js, val, pointerof(size))
-        String.new(buffer, size)
-      when 5 # Elk::JsTypes::JS_NUM
-        Elk.js_getnum(val)
-      end
-    end
-  end
-end
 
 @[Link(ldflags: "#{__DIR__}/elk.o")]
 lib Elk
@@ -85,15 +50,68 @@ lib Elk
   fun js_getstr(js : Js, val : Jsval, len : LibC::SizeT*) : LibC::Char*
 end
 
+module Js
+  VERSION = "0.1.0"
+
+  # A Javascript Engine
+  class Engine
+    def initialize(size : Int32 = 8192)
+      @js = Elk.js_create((" "*size), size)
+    end
+
+    def set_global(name : String, value : Elk::Jsval)
+      Elk.js_set(@js, Elk.js_glob(@js), name, value)
+    end
+
+    def eval(code : String)
+      Elk.js_eval(@js, code, code.bytesize)
+    end
+
+    def make_func(&)
+    end
+
+    def self.from_jsval(js : Elk::Js, val : Elk::Jsval)
+      # FIXME: handle JS_UNDEFINED, JS_ERR, JS_PRIV
+      case Elk.js_type(val)
+      when 1 # Elk::JsTypes::JS_NULL
+        nil
+      when 2 # Elk::JsTypes::JS_TRUE
+        true
+      when 3 # Elk::JsTypes::JS_FALSE
+        false
+      when 4 # Elk::JsTypes::JS_STR
+        size : UInt64 = 0
+        buffer = Elk.js_getstr(js, val, pointerof(size))
+        String.new(buffer, size)
+      when 5 # Elk::JsTypes::JS_NUM
+        Elk.js_getnum(val)
+      end
+    end
+  end
+
+  # Wraps a block in a function that can be called from Javascript
+  macro func(block)
+    Elk.js_mkfun(->(js : Elk::Js, args : Elk::Jsval*, nargs : UInt32) : Elk::Jsval {
+      p_args = (0...nargs).map { |i|
+        Js::Engine.from_jsval(js, args[i])
+      }
+
+      {{block}}.call(p_args)
+    })
+  end
+
+  alias Args = Array(String | Bool | Nil | Float64)
+
+  UNDEF = Elk.js_mkundef
+end
+
 js = Js::Engine.new
 
-js_print = ->(_js : Elk::Js, args : Elk::Jsval*, nargs : UInt32) : Elk::Jsval {
-  (0...nargs).each do |i|
-    puts Js::Engine.from_jsval(_js, args[i])
+js.set_global("print", Js.func(->(args : Js::Args) {
+  args.each do |arg|
+    puts arg
   end
-  Elk.js_mkundef
-}
+  Js::UNDEF
+}))
 
-js.set_global("print", Elk.js_mkfun(js_print))
-code = "print('Hello, World!', 42);"
-js.eval(code)
+js.eval("print('Hello, World!', 42);")
